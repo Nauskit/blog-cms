28decc1738cd0e76a586faa6664453e79bb13fef
Nutthakit <nutthakit2541@hotmail.com>
2025-03-15 22:18:55 +0700

    navbar-structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+export function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="px-5 py-3 bg-cyan-500">
+          <div className="flex justify-between">
+            <div className="text-xl">Logo</div>
+            <div className="text-lg">X</div>
+          </div>
+          <ul className="flex-col text-center p-3">
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
